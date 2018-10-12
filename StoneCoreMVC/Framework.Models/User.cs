b5084941a2296d410b0ca8b7e961f0611fa5b0e7
Dream <Dream@DESKTOP-FCV9BGF>
2018-10-12 17:00:36 +0800

    model dal

---
+++ StoneCoreMVC/Framework.Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Framework.Models
+{
+    public class User
+    {
+        private Guid id;
+        private string username;
+        private string password;
+
+        public Guid Id { get => id; set => id = value; }
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+    }
+}
